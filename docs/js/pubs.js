17aa683cf6478a5a90bf05c3c27e20cb9f06bd40
Stephane Letz <letz@grame.fr>
2024-03-30 23:17:11 +0100

    Revert to old URL.

--- docs/js/pubs.js
+++ docs/js/pubs.js
@@ -156,8 +156,8 @@ function getMenu(content) {
 const pubdiv = document.getElementById("publications");
 if (pubdiv) {
     let menu = getMenu("menuholder");
-    //const url = "https://api.archives-ouvertes.fr/search/FAUST" 
-    const url = "https://api.archives-ouvertes.fr/search/index?q=FAUST"
+    const url = "https://api.archives-ouvertes.fr/search/FAUST"
+    //const url = "https://api.archives-ouvertes.fr/search/index?q=FAUST"
     const options = "indent=true&wt=json&rows=2000&fl=*&sort=publicationDateY_i%20desc";
     getPublications(url + "?" + options)
         .then((result) => { display(result, pubdiv, menu); })
